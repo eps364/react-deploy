5b46b41b2be0630254bd001ee44a8b98ed41a7d3
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2020-07-14 15:13:43 -0300

    Updates

---
+++ precache-manifest.97a77598998835d4c80aed47f5822e96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1730dafd31a25a220f72360ce15308e",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "12858beb3f543ee991c6",
+    "url": "/react-deploy/static/css/main.28cf0b8d.chunk.css"
+  },
+  {
+    "revision": "b451c962521998ca2b46",
+    "url": "/react-deploy/static/js/2.38f1524e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.38f1524e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12858beb3f543ee991c6",
+    "url": "/react-deploy/static/js/main.7ec1aacd.chunk.js"
+  },
+  {
+    "revision": "4a8bebede5974b130c4b",
+    "url": "/react-deploy/static/js/runtime-main.fb319748.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
